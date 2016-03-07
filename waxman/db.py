c5bb3767d6cc6d9c330ac63920c1d371d96384d5
Mabry Cervin <mpcervin@uncg.edu>
2016-03-07 12:16:15 -0500

    Added initial simple db schema

--- waxman/db.py
+++ waxman/db.py
@@ -1,6 +1,19 @@
-import sqlalchemy
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Wax(Base):
+  __tablename__ = 'wax'
+
+  id = sa.Column(sa.Integer, primary_key=True)
+  albumName = sa.Column(sa.String)
+  artistName = sa.Column(sa.String)
+
+  def _repr_(self):
+    return "<Album:%s - Artist:%s>" % (self.albumName, self.artistName)
 
 class db:
   def __init__(self):
     # Connect to engine, here we use a temp Sqlite engine for development
-    engine = create_engine('sqlite:///:memory:', echo=True)
+    engine = sa.create_engine('sqlite:///:memory:', echo=True)
